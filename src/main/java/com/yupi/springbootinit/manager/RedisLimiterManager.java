284564d40e194ddeb942001fdb711061d1b74125
R-icher <1592322194@qq.com>
2025-08-09 15:28:29 +0800

    用Redisson的令牌桶算法实现限流

--- src/main/java/com/yupi/springbootinit/manager/RedisLimiterManager.java
+++ src/main/java/com/yupi/springbootinit/manager/RedisLimiterManager.java
@@ -21,15 +21,20 @@ public class RedisLimiterManager {
      * 限流操作
      */
     public void doPateLimit(String key){
-        // 创建一个名称为user_limiter的限流器，每秒最多访问 2 次
+        // 创建限流器，key 为每个用户唯一的id
         RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
-        // 限流器的统计规则(每秒2个请求;连续的请求,最多只能有1个请求被允许通过)
-        // RateType.OVERALL表示速率限制作用于整个令牌桶,即限制所有请求的速率
+        // 配置限流器，设置为每秒两次请求（即往令牌桶内每秒放入两个令牌）
         rateLimiter.trySetRate(RateType.OVERALL, 2, Duration.ofSeconds(1));
-        // 每当一个操作来了后，请求一个令牌
+
+        // 来一次操作，就请求一次令牌
+        /**
+         * 这里设置一次性拿多少个令牌，可以设置 `vip` 用户和普通用户，根据桶内令牌的数量，用户去拿取令牌，
+         * `vip`用户可以一次性请求1个令牌【这样流量就会更多，可以申请更多次key】。
+         * 普通用户一次性请求5个令牌【这样从桶中获取令牌的时候就只能拿几次就被限流了】
+         */
         boolean canOp = rateLimiter.tryAcquire(1);
-        // 如果没有令牌,还想执行操作,就抛出异常
-        if (!canOp) {
+        // 如果没有拿到令牌还想执行操作就抛出异常
+        if(!canOp){
             throw new BusinessException(ErrorCode.TOO_MANY_REQUEST);
         }
     }
