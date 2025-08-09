7b93ee310042cbc277d6a9b8052437abdacda51d
R-icher <1592322194@qq.com>
2025-08-09 20:25:12 +0800

    Revert "死信队列处理被消息队列拒绝的消息"
    This reverts commit 0fb9b4edce9738734792a2e3d201404a3c39ec2d.

--- src/main/java/com/yupi/springbootinit/bizmq/BIMqConstant.java
+++ src/main/java/com/yupi/springbootinit/bizmq/BIMqConstant.java
@@ -6,9 +6,4 @@ public interface BIMqConstant {
     String BI_QUEUE_NAME = "bi_queue";
 
     String BI_ROUTING_KEY = "bi_routingKey";
-
-    // 新增死信队列常量
-    String BI_DLX_EXCHANGE_NAME = "bi_dlx_exchange";
-    String BI_DLX_ROUTING_KEY = "bi_dlx_routingKey";
-    String BI_DLX_QUEUE_NAME = "bi_dlx_queue";
 }
